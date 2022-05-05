89b86cf3fd423731087cb4276ce0e11a7b840c2b
Juan Alegría <juanszalegria@gmail.com>
2022-05-05 15:49:15 -0500

    Style: fix spacing

--- ProblemaP1.py
+++ ProblemaP1.py
@@ -2,27 +2,28 @@
 # Juan Sebastián Alegría (202011282)
 # Proyecto 2
 
-import sys, math
+import sys
+import math
 
 # Función solución
 def grafosConectados(data):
-    num,indexG=1,0 #numeros de componentes conectados y el mayor indice anterior
-    subGraphs=[] #subcomponentes conectados
-    subGraphs.append([data[indexG],math.inf])
-    for i in range(1,len(data)):#Se generan los subcomponentes conectados
-        if data[i]>data[indexG]:
-            subGraphs.append([data[i],math.inf])
-            num=num+1
-            indexG=i
+    num, indexG = 1, 0  # Números de componentes conectados y el mayor índice anterior
+    subGraphs = []  # Subcomponentes conectados
+    subGraphs.append([data[indexG], math.inf])
+    for i in range(1, len(data)):  # Se generan los subcomponentes conectados
+        if data[i] > data[indexG]:
+            subGraphs.append([data[i], math.inf])
+            num = num+1
+            indexG = i
         else:
-            if data[i]<subGraphs[len(subGraphs)-1][1]:
-                subGraphs[len(subGraphs)-1][1]=data[i]
-    men=math.inf
-    for i in range(len(subGraphs)-1,0,-1):#Sobre conectan los subcomponentes conectados
-        if subGraphs[i][1]<men:
-            men=subGraphs[i][1]
-        if subGraphs[i-1][0]>men:
-            num=num-1
+            if data[i] < subGraphs[len(subGraphs)-1][1]:
+                subGraphs[len(subGraphs)-1][1] = data[i]
+    men = math.inf
+    for i in range(len(subGraphs)-1, 0, -1):  # Sobre conectan los subcomponentes conectados
+        if subGraphs[i][1] < men:
+            men = subGraphs[i][1]
+        if subGraphs[i-1][0] > men:
+            num = num-1
     return num
 
 
